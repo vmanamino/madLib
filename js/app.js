0d32d9c20207f0ca0825aebfcee4ff1f42463826
vmanamino <vmanamino@gmail.com>
2016-04-20 03:15:29 +0000

    controller with functions

---
+++ js/app.js
@@ -0,0 +1,38 @@
+angular.module("myApp", [])
+    .controller("myCtrl", function($scope){
+        $scope.name = '';
+        $scope.job_title = '';
+        $scope.tedious_task = '';
+        $scope.dirty_task = '';
+        $scope.celebrity = '';
+        $scope.useless_skill = '';
+        $scope.adjective = '';
+        $scope.obnoxious_celebrity = '';
+        $scope.huge_number = '';
+        $scope.gender = '';
+        $scope.setNominative = function(){
+            if ($scope.gender == 'female'){
+                return "she";
+            }
+            if ($scope.gender == 'male'){
+                return "he";
+            }
+        }
+        $scope.setInflected = function(){
+            if ($scope.gender == 'female'){
+                return "her";
+            }
+            if ($scope.gender == 'male'){
+                return "his";
+            }
+        }
+        $scope.setObject = function(){
+            if ($scope.gender == 'female'){
+                return "her";
+            }
+            if ($scope.gender == 'male'){
+                return "him";
+            }
+        }
+        
+    })
